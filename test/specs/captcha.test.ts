a11fb16ada15370bf8f5ff0af0f9b078d499758e
Simple <varg616@gmail.com>
2023-10-02 20:10:48 +0400

    cleaned a bit

--- test/specs/captcha.test.ts
+++ test/specs/captcha.test.ts
@@ -1,8 +1,6 @@
 describe("My Login application", () => {
     it("should login with valid credentials", async () => {
         await browser.url("https://www.google.com/recaptcha/api2/demo");
-        // await browser.url("https://phptravels.org/login");
-        // await browser.url("https://test-form-s.netlify.app");
 
         const frame = await $('iframe[title="reCAPTCHA"]');
         await browser.switchToFrame(frame);
@@ -13,11 +11,6 @@ describe("My Login application", () => {
         await browser.pause(3000);
 
         await browser.switchToParentFrame();
-        // <button class="rc-button goog-inline-block rc-button-audio" title="Get an audio challenge" value="" id="recaptcha-audio-button" tabindex="1"></button>
-        // <button class="rc-button-default goog-inline-block" title="" value="" id=":2" tabindex="0" aria-labelledby="audio-instructions rc-response-label">PLAY</button>
-        // <input type="text" id="audio-response" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" dir="ltr" class="rc-response-input-field label-input-label" aria-labelledby="rc-response-input-label">
-        // <button class="rc-button-default goog-inline-block" title="" value="" id="recaptcha-verify-button" tabindex="0">Verify</button>
-        // <audio id="audio-source" src="https://www.google.com/recaptcha/api2/payload?p=06AFcWeA5YOfpGkz2DyuymzU0t-Dd0hHjf6B2--nRIx9bY58LAUFhZiBSZDdrddvTCC9U-MeD6yu0UPCb0vHWwQjk2RY69PDn5nhDaSVWULYLM9ttmXyMjY9FXim6-rO35uHcuyibwvtKK5owjjIfcbZdkp-VnSax82wpjJQdFVb0e9OKQ9XapPBNrh-4wGdOdrkG2zBYI0JjGjwApSbFGqCiJmBbbpka9xIfvMdPUSJK_70MCvLkogMw&amp;k=6LfCJLceAAAAAJ_1NHWzOK2v5Uu60D5aQ6ACiq4R" style="display: none"></audio>
 
         const frame2 = await $('iframe[title^="recaptcha"]');
         await frame2.waitForDisplayed();
